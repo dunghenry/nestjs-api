e47ada5b5cefbdafcad2ee42161a5f8a29dfde88
dunghenry <trandungksnb6930082001@gmail.com>
2022-07-02 22:47:04 +0700

    add prisma and create model

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,7 +1,9 @@
 import { Injectable } from '@nestjs/common';
-
+import { User, Bookmark } from '@prisma/client';
+import { PrismaService } from 'src/prisma/prisma.service';
 @Injectable({})
 export class AuthService {
+  constructor(private prisma: PrismaService) {}
   signup() {
     return { username: 'Tran Van Dung' };
   }
---
+++ src/user/user.controller.ts
@@ -0,0 +1,5 @@
+import { Controller } from '@nestjs/common'
+
+@Controller({})
+// eslint-disable-next-line prettier/prettier
+export class AuthController { }
